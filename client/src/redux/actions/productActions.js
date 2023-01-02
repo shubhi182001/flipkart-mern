fa496846d69f37a106596b7c70d8156c6f57ff0b
shubhi182001 <kediashubhi@gmail.com>
2023-01-02 14:57:09 +0530

    product details redux done

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -12,10 +12,12 @@ export const getProducts = () => async(dispatch) => {
     }
 }
 
-export const getProductDetails = () => async(dispatch) => {
+export const getProductDetails = (id) => async(dispatch) => {
     try{
-        
+        dispatch({type: actionTypes.GET_PRODUCT_DETAILS_REQUEST})  ;
+        const {data} = await axios.get(`${URL}/product/${id}`);
+        dispatch({type: actionTypes.GET_PRODUCTS_SUCCESS, payload: data})
     }catch(e){
-        
+        dispatch({type: actionTypes.GET_PRODUCT_DETAILS_FAIL, payload: e.message})
     }
 }
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from "redux"
 import {composeWithDevTools} from 'redux-devtools-extension'; //to connect with chrome redux extension
 import thunk from 'redux-thunk';
-import { getProductReducer } from "./reducers/productReducer";
+import { getProductDetailsReducer, getProductReducer } from "./reducers/productReducer";
 
 
 const reducer = combineReducers({
-    getProducts: getProductReducer
+    getProducts: getProductReducer,
+    getProductDetailsReducer:getProductDetailsReducer
 });
 
 const middleware = [thunk];
